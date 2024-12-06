cb7bbe2d7f91f22fbedbb2365eba7e7c828f9f80
k-marian-deepak <kmariandeepak@gmail.com>
2024-12-06 16:52:14 +0530

    Error Corrections fpr Easy Statements in BugHuntter

--- problems/easy/easy_q17.py
+++
@@ -1,9 +0,0 @@
-# Print X N Times
-def print_x_n_times(x, n):
-    for i in range(1, n):  # Bug: Loop runs one less time than expected
-        print(x)
-if __name__ == "__main__":
- # Handle the input  by Yourself
- x=input("Enter a character:")
- n=int(input("enter the number of times to loop:"))
- print_x_n_times(x,n)
